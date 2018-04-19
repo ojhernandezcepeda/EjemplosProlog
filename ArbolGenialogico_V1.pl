787f5321902848e11f46658ea01518a00b8e9f53
jeffert025 <36453425+jeffert025@users.noreply.github.com>
2018-04-19 12:27:17 -0500

    Update and rename ArbolGenialogico.pl to ArbolGenialogico_V1.pl

--- ArbolGenialogico_V1.pl
+++ ArbolGenialogico_V1.pl
@@ -6,11 +6,9 @@ padrede('luis oscar','deidy').
 padrede('milton','luis').
 padrede('luis jr','luis').
 padrede('luis','raimundo').
-padrede('raimundo','simon').
 
-
-% ! papa, hijo, nieto,abuelo, bisnieto,bisabuelo, tio, sobrino,
-% hermano, primo
+% Version_1: papa, hijo, nieto,abuelo, bisnieto,bisabuelo, tio,
+% sobrino, hermano, primo
 
 hijode(A,B) :- padrede(B,A).
 abuelode(A,B) :- hijode(B,C),hijode(C,A).
@@ -18,3 +16,6 @@ nietode(A,B):-abuelode(B,A).
 bisabuelode(A,B):-abuelode(A,C),padrede(C,B).
 bisnietode(A,B):- bisabuelode(B,A).
 hermanode(A,B):- padrede(A,C), padrede(B,C),A\==B.
+tiode(A,B):- padrede(A,C), hermanode(C,B).
+sobrinode(A,B):- tiode(B,A).
+primode(A,B):- abuelode(A,C), abuelode(B,C), not(hermanode(A,B)), A\==B.
